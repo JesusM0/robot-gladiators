af7a9484d48df9f39996d5511144e702539d2bd1
Kos-Mos <ChuyM203@gmail.com>
2020-06-16 00:16:50 -0400

    added playerName/console logged

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+var playerName = window.prompt("Name Thy Mech!");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our Mech's name is... " + playerName);
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
